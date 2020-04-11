4279caa1f2b51181426a22ea2e9821d3178c3f8e
王青山 <wangqingshan.wqs@bytedance.com>
2020-04-11 16:53:41 +0800

    first commit

---
+++ spark_test/word_count.py
@@ -0,0 +1,18 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+if __name__ == "__main__":
+    spark = SparkSession\
+    .builder\
+    .appName('WordCount')\
+    .getOrCreate()
+
+    lines = spark.read.text("sample.txt")
+    wordCounts = lines\
+    .select(explode(split(lines.value, " "))
+            .alias("word"))\
+    .groupBy("word")\
+    .count()
+    wordCounts.show()
+
+    spark.stop()
